100798b5f025be5fd18f3db6d122669baf527d13
tolliverdanielle <tolliver.da@northeastern.edu>
2022-04-10 13:21:12 -0400

    updating server.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import paintingsController from "./controllers/wiki-art-controller.js"
-
+const mongoose = require('mongoose');
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/webdev');
 
 app.use(cors());
 app.use(express.json());
+app.use(session({
+    secret: 'SECRET',
+    cookie: {secure: false}
+}));
 
-paintingsController(app);
+require("./controllers/users-controller")(app);
+require("./controllers/auth-controller")(app);
+require("./controllers/wiki-art-controller")(app);
+require("./controllers/collection-controller")(app);
+require("./controllers/comments-controller")(app);
 
 app.listen(process.env.PORT || 4000);
