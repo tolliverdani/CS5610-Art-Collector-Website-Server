2d90fbbecb931553eb4bfb61e34e6cba162a98cf
tolliverdanielle <tolliver.da@northeastern.edu>
2022-04-17 23:29:17 -0400

    trying to fix mongoose issue

--- database/users/users-dao.js
+++ database/users/users-dao.js
@@ -1,24 +1,19 @@
 import userModel from "./users-model.js";
 
 const findAllUsers = () => userModel.find()
-
 const findUserById = (id) => userModel.findById(id)
-
 const findUserByEmail = (email) => userModel.findOne({email})
-
 const findUserByCredentials = (email, password) => userModel.findOne({email, password})
-
 const createUser = (user) => userModel.create(user)
-
 const deleteUser = (id) => userModel.deleteOne({_id: id})
-
-const updateUser = (id, user) => userModel.updateOne(
-    {_id: id},
-    {$set: user}
-)
+const updateUser = (id, user) => userModel.updateOne({_id: id}, {$set: user})
 
 export default {
-    findAllUsers, findUserById,
-    findUserByEmail, findUserByCredentials,
-    createUser, deleteUser, updateUser
+    findAllUsers,
+    findUserById,
+    findUserByEmail,
+    findUserByCredentials,
+    createUser,
+    deleteUser,
+    updateUser
 }
